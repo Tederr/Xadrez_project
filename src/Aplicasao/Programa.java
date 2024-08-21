1d41fbf6b01aad4cd15701da38d6a6d73a66ad7e
Tederr <jp3351207@gmail.com>
2024-08-20 21:43:27 -0300

    Primeira classe: Posicao

--- src/Aplicasao/Programa.java
+++ src/Aplicasao/Programa.java
@@ -1,9 +1,12 @@
 package Aplicasao;
 
+import quadroDeTabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("ola");
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 
 	}
 
